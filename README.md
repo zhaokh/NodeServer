af87221935b341ef62378e721c957df735fe9ca7
zhaokh <wwwzhaokh@126.com>
2018-12-15 14:10:20 +0800

    添加router路由

---
+++ README.md
@@ -0,0 +1,2 @@
+安装淘宝cnpm：  npm install -g cnpm --registry=https://registry.npm.taobao.org
+js文件如果保存为unicode格式，则nodejs启动会报错，保存为utf-8格式，则成功
--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-var http = require('http');
-
-http.createServer(function (request, response) {
-
-    // 发送 HTTP 头部 
-    // HTTP 状态值: 200 : OK
-    // 内容类型: text/plain
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-
-    // 发送响应数据 "Hello World"
-    response.end('Hello World\n');
-}).listen(8888);
-
-// 终端打印如下信息
-console.log('Server running at http://127.0.0.1:8888/');
+var http = require("http");
+var url = require("url");
+ 
+function start(route) {
+  function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
+ 
+    route(pathname);
+ 
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+  }
+ 
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started.");
+}
+ 
+exports.start = start;
