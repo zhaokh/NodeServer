932524efe6e52fd75c49ce34941ccca3792c3c8b
zhaokh <wwwzhaokh@126.com>
2018-12-15 15:21:55 +0800

    POST

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+ 
+// 创建 application/x-www-form-urlencoded 编码解析
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
  
 app.use(express.static('public'));
  
@@ -7,12 +11,12 @@ app.get('/index.html', function (req, res) {
    res.sendFile( __dirname + "/" + "index.html" );
 })
  
-app.get('/process_get', function (req, res) {
+app.post('/process_post', urlencodedParser, function (req, res) {
  
    // 输出 JSON 格式
    var response = {
-       "first_name":req.query.first_name,
-       "last_name":req.query.last_name
+       "first_name":req.body.first_name,
+       "last_name":req.body.last_name
    };
    console.log(response);
    res.end(JSON.stringify(response));
