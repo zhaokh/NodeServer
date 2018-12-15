29e6a3f982b01b1dfa81f9e635b4041587e28520
zhaokh <wwwzhaokh@126.com>
2018-12-15 15:17:50 +0800

    express_demo3,GET

--- server.js
+++ server.js
@@ -1,35 +1,28 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
+var express = require('express');
+var app = express();
  
+app.use(express.static('public'));
  
-// 创建服务器
-http.createServer( function (request, response) {  
-   // 解析请求，包括文件名
-   var pathname = url.parse(request.url).pathname;
-   
-   // 输出请求的文件名
-   console.log("Request for " + pathname + " received.");
-   
-   // 从文件系统中读取请求的文件内容
-   fs.readFile(pathname.substr(1), function (err, data) {
-      if (err) {
-         console.log(err);
-         // HTTP 状态码: 404 : NOT FOUND
-         // Content Type: text/plain
-         response.writeHead(404, {'Content-Type': 'text/html'});
-      }else{             
-         // HTTP 状态码: 200 : OK
-         // Content Type: text/plain
-         response.writeHead(200, {'Content-Type': 'text/html'});    
-         
-         // 响应文件内容
-         response.write(data.toString());        
-      }
-      //  发送响应数据
-      response.end();
-   });   
-}).listen(8888);
+app.get('/index.html', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.html" );
+})
  
-// 控制台会输出以下信息
-console.log('Server running at http://127.0.0.1:8888/');
+app.get('/process_get', function (req, res) {
+ 
+   // 输出 JSON 格式
+   var response = {
+       "first_name":req.query.first_name,
+       "last_name":req.query.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+ 
+var server = app.listen(8081, function () {
+ 
+  var host = server.address().address
+  var port = server.address().port
+ 
+  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+ 
+})
